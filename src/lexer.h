f1e3340a9864391999f7a2c5a646c234712475a1
Jacob Carlborg <doob@me.com>
2014-10-18 17:46:41 +0200

    Rempve DMD_OBJC from tokens in lexer.

--- src/lexer.h
+++ src/lexer.h
@@ -184,13 +184,11 @@ enum TOK
 
         TOKinterval,
 
-//#if DMD_OBJC
         TOKobjcclsref, // Objective-C class reference
         TOKobjc_dotclass,
         TOKobjc_dotprotocolof,
         
         TOKobjcselector, // selector keyword
-//#endif
 
         TOKMAX
 };
